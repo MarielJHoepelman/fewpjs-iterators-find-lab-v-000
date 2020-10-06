5087f8d5f2d5a1ca1c52ad7cdcc3a9c54edf3599
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-06 21:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function testFunc() {
   return "hi"
 }
 
-const superbowlWin = record => record.find(element => element.result === "W");
+const superbowlWin = record => record.find(element => {
+  element.result === "W"
+});
